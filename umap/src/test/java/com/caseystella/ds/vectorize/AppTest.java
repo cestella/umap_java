ab7942439a8a275a4f59f52110714f38b7afd01c
Casey Stella <cstella@stripe.com>
2019-06-20 19:09:42 -0400

    Proper structure

--- umap/src/test/java/com/caseystella/ds/vectorize/AppTest.java
+++ umap/src/test/java/com/caseystella/ds/vectorize/AppTest.java
@@ -3,6 +3,10 @@ package com.caseystella.ds.vectorize;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.eigen.Eigen;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.ops.transforms.Transforms;
 
 /**
  * Unit test for simple App.
@@ -15,6 +19,13 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        INDArray twoDArray = Nd4j.rand(new int[] {100, 100}).mul(100);
+        INDArray eigen = Eigen.symmetricGeneralizedEigenvalues(twoDArray);
+        print("Eigenvalues:", eigen);
+    }
+    private static void print(String tag, INDArray arr) {
+        System.out.println("----------------");
+        System.out.println(tag + ":\n" + arr.toString());
+        System.out.println("----------------");
     }
 }
